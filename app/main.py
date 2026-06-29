8ae05e333313a589d8c02f56b7f25923f0d13327
pablodgs <pablo-dgs1@hotmail.com>
2026-06-29 17:42:25 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,11 +8,19 @@ def main():
 
         # Read user input from standard input
         user_input = sys.stdin.readline().strip()
-
-        if user_input == "exit":
-            break
+        user_words_list = user_input.split()
+        if len(user_words_list) > 0:
+            user_command = user_words_list[0]
+            raw_user_args = user_input[len(user_command)+1:]
         else:
-            sys.stdout.write(f"{user_input}: command not found\n")
+            user_command = ""
+            raw_user_args = ""
+        if user_command == "exit":
+            break
+        elif user_command == "echo":
+            sys.stdout.write(f"{raw_user_args}\n")
+        else:
+            sys.stdout.write(f"{user_command}: command not found\n")
 
 
 if __name__ == "__main__":
