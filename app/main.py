a12d6305ddc1f7abb3ea1f1c563e3ddd3d000d5c
pablodgs <pablo-dgs1@hotmail.com>
2026-06-29 17:19:29 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,10 +5,9 @@ def main():
     sys.stdout.write("$ ")
 
     # Read user input from standard input
-    user_input = sys.stdin.readline()
-    user_command = user_input
+    user_input = sys.stdin.readline().strip()
 
-    sys.stdout.write(f"{user_command}: command not found\n")
+    sys.stdout.write(f"{user_input}: command not found\n")
 
     pass
 
